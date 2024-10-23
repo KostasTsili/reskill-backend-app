cdbd4a2cd29994d9eb5a5e39daa38251ea04c290
Konstantinos Tsililis <89405783+KostasChili@users.noreply.github.com>
2024-10-23 12:58:20 +0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://reskill-frontend-app.vercel.app/"
+  "https://reskill-frontend-app.vercel.app/",
+  "3.75.158.163",
+  "3.125.183.140",
+  "35.157.117.28"
 ]
 
 
